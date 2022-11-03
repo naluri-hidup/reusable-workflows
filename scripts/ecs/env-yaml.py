8afe79e35ce7ed7e765e2e1e41ea9b728b005c6f
Lee You Jing <youjing@Lees-MacBook-Pro.local>
2022-11-03 18:37:10 +0800

    update

--- scripts/ecs/env-yaml.py
+++ scripts/ecs/env-yaml.py
@@ -5,15 +5,15 @@ import argparse
 
 arg_parser = argparse.ArgumentParser(description='EnvYaml')
 arg_parser.add_argument('-t', '--template')
-
+arg_parser.add_argument('-o', '--output')
 args = arg_parser.parse_args()
 
 dir_path = os.getcwd()
 
 # Use the task definition template
-env = EnvYAML(dir_path + '/template/ecs-td-template/' + args.template + '.yml')
+env = EnvYAML(dir_path + '/' + args.template)
 
-with open(dir_path + '/task-definition.yml', "w", encoding = "utf-8") as yaml_file:
+with open(dir_path + '/' + args.output, "w", encoding = "utf-8") as yaml_file:
     dump = ruamel.yaml.dump(env['task-definition'], Dumper=ruamel.yaml.RoundTripDumper)
     yaml_file.write(dump)
 
---
+++ scripts/ecs/insert-yaml.py
@@ -0,0 +1,24 @@
+import os
+import argparse
+import ruamel.yaml
+
+arg_parser = argparse.ArgumentParser(description='Insert env and secrets into yaml')
+arg_parser.add_argument('-e', '--environment')
+arg_parser.add_argument('-s', '--source')
+arg_parser.add_argument('-d', '--destination')
+arg_parser.add_argument('-o', '--output')
+yaml = ruamel.yaml.YAML()
+dir_path = os.getcwd()
+
+with open(dir_path + "/" + args.destination) as ad:
+    data = yaml.load(ad)
+
+with open(dir_path + "/" + args.source) as ad:
+    data2 = yaml.load(ad)
+
+data['task-definition']['containerDefinitions'][0]['environment'] = data2['prod']['environment']
+data['task-definition']['containerDefinitions'][0]['secrets'] = data2['prod']['secrets']
+
+with open(dir_path + "/" + args.output, "w", encoding = "utf-8") as yaml_file:
+    dump = ruamel.yaml.dump(data, Dumper=ruamel.yaml.RoundTripDumper)
+    yaml_file.write(dump)
