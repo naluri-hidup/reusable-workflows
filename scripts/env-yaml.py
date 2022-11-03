a76a9ff0dac6de67fe49b9452a396b4d2211782a
Lee You Jing <youjing@Lees-MacBook-Pro.local>
2022-11-03 13:24:40 +0800

    update

--- scripts/env-yaml.py
+++ scripts/env-yaml.py
@@ -1,13 +1,19 @@
 from envyaml import EnvYAML
 import ruamel.yaml
 import os 
+import argparse
+
+arg_parser = argparse.ArgumentParser(description='EnvYaml')
+arg_parser.add_argument('-t', '--template')
+
+args = arg_parser.parse_args()
 
 dir_path = os.getcwd()
-# read file env.yaml and parse config
-env = EnvYAML(dir_path + '/template/ecs-td-template/template.yml')
+# Use the task definition template
+env = EnvYAML(dir_path + '/template/ecs-td-template/' + args.template + '.yml')
 
 
-with open(dir_path + '/after.yml', "w", encoding = "utf-8") as yaml_file:
+with open(dir_path + '/task-definition.yml', "w", encoding = "utf-8") as yaml_file:
     dump = ruamel.yaml.dump(env['task-definition'], Dumper=ruamel.yaml.RoundTripDumper)
     yaml_file.write(dump)
 
