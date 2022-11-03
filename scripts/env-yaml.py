de4d25991a703c4f3f27ec03e233612fc3f585c2
Lee You Jing <youjing@Lees-MacBook-Pro.local>
2022-11-03 11:19:59 +0800

    update

---
+++ scripts/env-yaml.py
@@ -0,0 +1,14 @@
+from envyaml import EnvYAML
+import ruamel.yaml
+import os 
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# read file env.yaml and parse config
+env = EnvYAML(dir_path + 'template/ecs-td-template/template.yml')
+
+
+with open(dir_path + 'after.yml', "w", encoding = "utf-8") as yaml_file:
+    dump = ruamel.yaml.dump(env['task-definition'], Dumper=ruamel.yaml.RoundTripDumper)
+    yaml_file.write(dump)
+
+print(env['task-definition'])
